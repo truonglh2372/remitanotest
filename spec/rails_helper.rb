73de5b36a0bd2c9beb0d558a7db935cd6242086f
truonglh <truong.lh2372@gmail.com>
2022-08-25 03:09:53 +0700

    add factory and test sign up feature

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
