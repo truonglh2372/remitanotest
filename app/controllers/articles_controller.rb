775eb1a78ff1ef8413597e798149847694f2c72b
truonglh <truong.lh2372@gmail.com>
2022-08-25 02:08:10 +0700

    fix devise confix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   def index; end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,8 +9,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.authentication_keys = [:username]
-  config.case_insensitive_keys = [:username]
   config.strip_whitespace_keys = [:username]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
