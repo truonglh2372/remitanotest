2c68cd0c0bb73a42b5489ef170d62b9e75b53ee8
truonglh <truong.lh2372@gmail.com>
2022-08-25 12:09:21 +0700

    add article youtube id and render

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
+require 'uri'
+
 class Article < ApplicationRecord
   belongs_to :user
+  before_validation :set_youtube_id_from_description
+
+  def set_youtube_id_from_description
+    reg_exp = %r{^.*((youtu.be/)|(v/)|(/u/\w/)|(embed/)|(watch\?))\??v?=?([^#&?]*).*}
+    urls_from_description = URI.extract(description)
+    self.youtube_id = urls_from_description[0].split(reg_exp).last if urls_from_description.size.positive?
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,23 +3,35 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  describe 'it create user' do
-    subject { Article.create(title: 'example article', description: '123456') }
-    it 'should create user successfully' do
+  describe 'it create article' do
+    let(:user) { create :user }
+    subject { user.articles.create!(title: 'example article', description: '123456') }
+    it 'should create article successfully' do
       expect do
         expect(subject).to eq Article.first
       end.to change(Article, :count).by(1)
     end
   end
 
-  describe 'it destroy user ' do
-    subject { Article.find_by(title: 'example article').destroy }
-    before do
-      Article.create(title: 'example article', description: '123456')
+  describe 'it destroy article ' do
+    let!(:article) { create :article }
+    subject { article.destroy }
+
+    it 'should create article successfully' do
+      expect { subject }.to change(Article, :count).by(-1)
+    end
+  end
+
+  describe 'it create article with youtube id from description' do
+    let!(:user) { create :user }
+    subject do
+      user.articles.create(title: 'example article',
+                           description: 'https://www.youtube.com/watch?v=H7Q9isRWNS4&ab_channel=RazyClips')
     end
 
-    it 'should create user successfully' do
-      expect { subject }.to change(Article, :count).by(-1)
+    it 'should create article successfully' do
+      res = subject
+      expect(res.youtube_id).to eq('H7Q9isRWNS4')
     end
   end
 end
